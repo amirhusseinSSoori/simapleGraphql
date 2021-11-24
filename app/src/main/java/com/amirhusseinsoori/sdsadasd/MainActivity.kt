28d2bd57f20d587a36f7514e2d085b8175f09a94
Soori <soori@hamtasharif.ir>
2021-11-24 07:13:11 -0800

    first commit

---
+++ app/src/main/java/com/amirhusseinsoori/sdsadasd/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.amirhusseinsoori.sdsadasd
+
+import FindQuery
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import kotlinx.android.synthetic.main.repo_layout.*
+
+import okhttp3.OkHttpClient
+import java.util.logging.Logger
+
+class MainActivity : AppCompatActivity() {
+    private val BASE_URL = "https://api.github.com/graphql"
+    private lateinit var client: ApolloClient
+
+    companion object {
+        val Log = Logger.getLogger(MainActivity::class.java.name)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        client = setupApollo()
+
+
+
+        button_find.setOnClickListener {
+            progress_bar.visibility = View.VISIBLE
+            client.query(FindQuery("amirhusseinSSoori","mapBoxWithKotlin") )
+
+
+                .enqueue(object : ApolloCall.Callback<FindQuery.Data>() {
+
+                    override fun onFailure(e: ApolloException) {
+                        Log.info(e.message.toString())
+                    }
+
+                    override fun onResponse(response: Response<FindQuery.Data>) {
+                        Log.info(" " + response.data()?.repository)
+                        runOnUiThread {
+                            progress_bar.visibility = View.GONE
+                            name_text_view.text = String.format(
+                                getString(R.string.name_text),
+                                response.data()?.repository?.name
+                            )
+                            description_text_view.text = String.format(
+                                getString(R.string.description_text),
+                                response.data()?.repository?.description
+                            )
+                            forks_text_view.text = String.format(
+                                getString(R.string.fork_count_text),
+                                response.data()?.repository?.forkCount.toString()
+                            )
+                            url_text_view.text = String.format(
+                                getString(R.string.url_count_text),
+                                response.data()?.repository?.url.toString()
+                            )
+                        }
+                    }
+
+                })
+        }
+
+    }
+
+    private fun setupApollo(): ApolloClient {
+        val okHttp = OkHttpClient
+            .Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val builder = original.newBuilder().method(
+                    original.method(),
+                    original.body()
+                )
+                builder.addHeader(
+                    "Authorization", "Bearer " + "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"
+                )
+                chain.proceed(builder.build())
+            }
+            .build()
+        return ApolloClient.builder()
+            .serverUrl(BASE_URL)
+            .okHttpClient(okHttp)
+            .build()
+    }
+}
